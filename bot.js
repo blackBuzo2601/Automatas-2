3747e8e6134a408c2961f72aff54d02023802040
blackBuzo2601 <elianpro58@gmail.com>
2024-02-29 17:07:45 -0800

    Implementada función que calcula numeros aleatorios entre 0 y un valor establecido, esto para que el bot lance mensajes aleatorios

--- bot.js
+++ bot.js
@@ -50,20 +50,22 @@ const bot = new Telegraf(token);
 
 //NUEVAS VARIABLES
 var contadorMensajesIguales=0;
-var randomDecimal = Math.random(); //usamos el objeto Math de JS y usamos el método random() que devuelve un numero aleatorio entre eo 0 y 1 en decimal.
-var randomEntero  = Math.floor(randomDecimal*100); //con el metodo floor del objeto Math, redondeamos un valor hacia abajo. Lo que devolverá un entero.
+var randomDecimal=0; //usamos el objeto Math de JS y usamos el método random() que devuelve un numero aleatorio entre eo 0 y 1 en decimal.
+var randomEntero=0;
 var mensajeRecibido=""   //almacena el mensaje recibido actualmente
 var mensajeAnterior="";  //almacenara el mensaje recibido anteriormente para comparar que sean diferentes
-
+var parametroNumerico=0;
 //LAS SIGUIENTES CONSTANTES ALMACENAN LA CANTIDAD DE OBJETOS QUE HAY EN CADA CLAVE DEL archivo arreglojuegos.JSON
 //Estas serán util para poder implementar una función para escoger un mensaje aleatorio estableciendo un tope maximo
 //que será la cantidad que hay. De esta manera si hay 21 mensajes diferentes, el metodo random devolvera entre 1 y 21.
 const listaVideojuegosCantidad = dataParseada.listaVideojuegos.length;
 const mensajesBienvenidaCantidad = dataParseada.mensajesBienvenida.length;
-const mensajesSalidaCantidad = dataParseada.mensajesSalida.length;
-const mensajesMensajeRepetidoCantidad = dataParseada.mensajesMensajeRepetido.length;
-const mensajesConsejosPersonalesCantidad = dataParseada.mensajesConsejosPersonales.length;
+const mensajesSalidaCantidad = dataParseada.mensajesSalida.length;                         
+const mensajesMensajeRepetidoCantidad = dataParseada.mensajesMensajeRepetido.length;      
+const mensajesConsejosPersonalesCantidad = dataParseada.mensajesConsejosPersonales.length; 
 ///------------------------------------------AREA DE LA MAGIA DEL BOT----------------------------------------------
+
+
 //ctx (context) hace referencia a los datos que se usan en un chat
 bot.on('message', (ctx) => {
    mensajeAnterior=mensajeRecibido; //almacenar el mensaje anterior.
@@ -82,20 +84,31 @@ bot.on('message', (ctx) => {
     
     if(contadorMensajesIguales>=2){ 
     ctx.reply(dataParseada.mensajesMensajeRepetido[5].mensaje);//reply es una función de ctx que responde al remitente.
-    }else{ //Aquí entra la primera vez que se envia un mensaje al bot.
+    }else{ //Aquí entra la primera vez que se envia un mensaje al bot. Aqui entra cuando no se enviaron 3 mensajes iguales 
     ctx.reply(dataParseada.mensajesBienvenida[1].mensaje);
     }
 
-});
+}); //fin de analizador de mensaje.
 
 
 
 //-----------------------------------------------------------------------------------------------------------------
-console.log("AREA DE PRUEBAS EN CONSOLA");
-console.log("AREA DE PRUEBAS EN CONSOLA");
-console.log("AREA DE PRUEBAS EN CONSOLA");
+console.log("PRUEBAS PRUEBAS PRUEBAS PRUEBAS PRUEBAS PRUEBAS PRUEBAS\n");
+mensajeAleatorio(mensajesConsejosPersonalesCantidad); //llamada la funcion de mensajeAleatorio
 
 
+
+
+//----------------------------DECLARACION DE FUNCIONES ADICIONALES--------------------------------------------------
+
+
+//FUNCION QUE ME PERMITIRÁ CALCULAR NUMEROS ALEATORIOS EN UN RANGO DEFINIDO para que mande un mensaje aleatorio
+function mensajeAleatorio(parametroNumerico){ //recibe por parametro el valor maximo del rango
+  randomDecimal=Math.random();                         //devuelve un valor aleatorio entre 0 y 1.
+  randomEntero=Math.floor(randomDecimal * parametroNumerico); //se multiplica el valor
+                                                //0.aleatorio por el parametro y al final se redondea hacia abajo
+  console.log("EL resultado aleatorio es: "+randomEntero);
+}
 
 
 
